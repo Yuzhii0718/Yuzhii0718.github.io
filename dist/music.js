d7d7b960eb9b6ca7d31e0f9ba9ac5a099bf7e842
Zongchen Li <lizongchen0718@gmail.com>
2023-03-14 22:54:39 +0800

    Site updated: 2023-03-14 22:54:38

--- dist/music.js
+++ dist/music.js
@@ -1,6 +1,6 @@
 const ap = new APlayer({
     container: document.getElementById('aplayer'),
-    fixed: false,
+    fixed: true,
     autoplay: false,
     theme: '#b7daff',
     loop: 'all',
@@ -8,24 +8,17 @@ const ap = new APlayer({
     preload: 'auto',
     volume: 0.7,
     mutex: true,
-    listFolded: false,
+    listFolded: true,
     listMaxHeight: '200px',
     lrcType: 3,
-    audio: [
-      {
-        name: "Plastic Love",
-//        artist: '情歌',
-        url: 'http://music.163.com/outchain/player?type=2&id=408328190',
-//        cover: '/music/cover/兩隻老虎.jpg',
-//        lrc: '/music/lyrics/兩隻老虎.lrc',
-      },
-/*      {
-        name: '哥哥爸爸真偉大',
-        artist: '兒歌',
-        url: 'https://XXX.mp3',
-        cover: '/music/cover/哥哥爸爸真偉大.jpg',
-        lrc: '/music/lyrics/哥哥爸爸真偉大.lrc'
-      }
-*/
-    ]
+    audio: 
+	[
+		{
+            name: 'Plastic Love (TARA Bootleg)',
+            artist: 'TARA',
+            url: 'http://m10.music.126.net/20230314215022/83f7f9fdf0fe76006647f17aa8f3c3cb/ymusic/da73/ab3e/8e80/e1faa1baba5c22092c76bcc43a91d30f.mp3',
+            cover: 'http://p1.music.126.net/Kg5_jhvDWx24dtrELHCXGA==/1394180748278530.jpg',
+			lrc: ''
+		}
+	]
 });
