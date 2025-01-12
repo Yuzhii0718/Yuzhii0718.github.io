a71002dd13119be5c772a58620da869facaac3a2
Free Everywhere <yuzhii0718@outlook.com>
2025-01-12 21:07:34 +0800

    Site updated: 2025-01-12 21:07:30

--- js/third-party/math/katex.js
+++ js/third-party/math/katex.js
@@ -1,2 +1,2 @@
 document.addEventListener("page:loaded",(()=>{CONFIG.enableMath&&NexT.utils.getScript(CONFIG.katex.copy_tex_js).catch((()=>{}))}));
-//# sourceMappingURL=lean-analytics.js.map
+//# sourceMappingURL=wavedrom.js.map
