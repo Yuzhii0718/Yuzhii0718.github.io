a35cc4b31b5693c434be0b188391a6e5f1d68358
Free Everywhere <yuzhii0718@outlook.com>
2025-04-10 17:03:23 +0800

    Site updated: 2025-04-10 17:03:14

--- js/third-party/comments/utterances.js
+++ js/third-party/comments/utterances.js
@@ -1,2 +1,17 @@
-document.addEventListener("page:loaded",(()=>{CONFIG.page.comments&&NexT.utils.loadComments(".utterances-container").then((()=>NexT.utils.getScript("https://utteranc.es/client.js",{attributes:{async:!0,crossOrigin:"anonymous",repo:CONFIG.utterances.repo,"issue-term":CONFIG.utterances.issue_term,theme:CONFIG.utterances.theme},parentNode:document.querySelector(".utterances-container")})))}));
-//# sourceMappingURL=katex.js.map
+/* global NexT, CONFIG */
+
+document.addEventListener('page:loaded', () => {
+  if (!CONFIG.page.comments) return;
+
+  NexT.utils.loadComments('.utterances-container')
+    .then(() => NexT.utils.getScript('https://utteranc.es/client.js', {
+      attributes: {
+        async       : true,
+        crossOrigin : 'anonymous',
+        'repo'      : CONFIG.utterances.repo,
+        'issue-term': CONFIG.utterances.issue_term,
+        'theme'     : CONFIG.utterances.theme
+      },
+      parentNode: document.querySelector('.utterances-container')
+    }));
+});
--- js/third-party/tags/wavedrom.js
+++ js/third-party/tags/wavedrom.js
@@ -1,2 +1,13 @@
-document.addEventListener("page:loaded",(()=>{NexT.utils.getScript(CONFIG.wavedrom.js,{condition:window.WaveDrom}).then((()=>{NexT.utils.getScript(CONFIG.wavedrom_skin.js,{condition:window.WaveSkin}).then((()=>{WaveDrom.ProcessAll()}))}))}));
-//# sourceMappingURL=katex.js.map
+/* global NexT, CONFIG, WaveDrom */
+
+document.addEventListener('page:loaded', () => {
+  NexT.utils.getScript(CONFIG.wavedrom.js, {
+    condition: window.WaveDrom
+  }).then(() => {
+    NexT.utils.getScript(CONFIG.wavedrom_skin.js, {
+      condition: window.WaveSkin
+    }).then(() => {
+      WaveDrom.ProcessAll();
+    });
+  });
+});
