8cab274fc4238978cee8c95ebfcae1d1394ae5db
Free Everywhere <yuzhii0718@outlook.com>
2025-05-17 21:07:16 +0800

    Site updated: 2025-05-17 21:07:13

--- js/third-party/comments/isso.js
+++ js/third-party/comments/isso.js
@@ -1,2 +1,2 @@
 document.addEventListener("page:loaded",(()=>{CONFIG.page.comments&&NexT.utils.loadComments("#isso-thread").then((()=>NexT.utils.getScript(`${CONFIG.isso}js/embed.min.js`,{attributes:{dataset:{isso:`${CONFIG.isso}`}},parentNode:document.querySelector("#isso-thread")})))}));
-//# sourceMappingURL=lean-analytics.js.map
+//# sourceMappingURL=pdf.js.map
